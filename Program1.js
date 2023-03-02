4cfd89eb3fabcb195d293691f9977f49b99bbad3
Sneha Y. Choudhari <snehachoudhari13@gmail.com>
2023-03-02 16:11:36 +0530

    First-code-done

---
+++ Program1.js
@@ -0,0 +1,8 @@
+// Write a java script program to print "Jay Ganesh" on screen.
+
+//----------------------------------------------
+// Description   :  log function is use to  Display the output.
+// Author        :  Sneha Yogesh Choudhari
+//----------------------------------------------
+
+console.log("Jay Ganesh");

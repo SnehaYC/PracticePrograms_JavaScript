d96f15778cb966f8898b9b2f3eedf0285f4c3f97
Sneha Y. Choudhari <snehachoudhari13@gmail.com>
2023-03-09 09:11:02 +0530

    updated Program_01

---
+++ Basic-Programs-II/Program_01.js
@@ -0,0 +1,9 @@
+//Accept number from user and print numbers up to that number.
+
+//----------------------------------------------
+// Function Name : Display
+// Input         : Integer
+// Output        : Integer
+// Description   : This funtion display the numbers.
+// Author        : Sneha Yogesh Choudhari
+//----------------------------------------------

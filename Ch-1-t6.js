3cb59e57d09bf2a6dc2d89c26f5dc3199d418f8b
Sneha Y. Choudhari <snehachoudhari13@gmail.com>
2023-04-02 17:18:01 +0530

    added closures code

---
+++ Ch-1-t6.js
@@ -0,0 +1,21 @@
+/** @format */
+
+const Person = ({ name, age, job }) => {
+  var _name = name;
+  var _age = age;
+  var _job = job;
+
+  return {
+    getName: () => _name,
+    getAge: () => _age,
+    getJob: () => _job,
+
+    setJob: (newJob) => (_job = newJob),
+  };
+};
+
+const me = Person({ name: "Sneha", age: 25, job: "developer" });
+console.log(me.getJob());
+me.setJob("senior developer");
+console.log(me.getJob());
+me._job; // undefined

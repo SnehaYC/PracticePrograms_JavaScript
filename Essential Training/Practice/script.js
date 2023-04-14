8960c0b767632f034d861e7145be8d24818b3417
Sneha Y. Choudhari <snehachoudhari13@gmail.com>
2023-04-14 20:52:31 +0530

    Basic setup done

---
+++ Essential Training/Practice/script.js
@@ -0,0 +1,32 @@
+/**
+ * Practice: Building objects
+ *
+ * - Create JavaScript objects based on objects in your current environment.
+ * - Give each object an identifiable name.
+ * - Create properties to describe the objects and set their values.
+ * - Find an object that has another object inside of it to create a nested object.
+ * - Test your objects in the browser console by accessing the entire object and its specific properties.
+ *
+ * @format
+ */
+
+const backpack = {
+  name: "Everyday Backpack",
+  volume: 30,
+  color: "grey",
+  pocketNum: 15,
+  strapLength: {
+    left: 26,
+    right: 26,
+  },
+  lidOpen: false,
+  toggleLid: function (lidStatus) {
+    this.lidOpen = lidStatus;
+  },
+  newStrapLength: function (lengthLeft, lengthRight) {
+    this.strapLength.left = lengthLeft;
+    this.strapLength.right = lengthRight;
+  },
+};
+
+console.log("The backpack object:", backpack);
--- Essential Training/README.md
+++ Essential Training/README.md
@@ -1,3 +1,6 @@
 <!-- @format -->
 
-### Introduction
+# Introduction
+
+- JavaScript is a prototype based object oriented programming language. That means, at its core, when we work with JavaScript, we're working with objects, and these objects are based on prototype.
+- JavaScript is a prototype based object oriented programming language. That means, each object is a unique instance of an object prototype.

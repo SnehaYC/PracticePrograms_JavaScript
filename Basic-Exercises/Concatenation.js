fdc3f738db876ac273d998cb9b1473daa9f60281
Sneha Y. Choudhari <snehachoudhari13@gmail.com>
2023-06-17 16:36:21 +0530

    Ex-2 done

---
+++ Basic-Exercises/Concatenation.js
@@ -0,0 +1,13 @@
+/** @format */
+
+// Concatenate two string in a function
+let name = "Linda";
+let greeting = "Hi there";
+
+// Create a function that logs out "Hi there, Linda!" when called
+
+function greetLinda() {
+  console.log(greeting + ", " + name + "!");
+}
+
+greetLinda();
